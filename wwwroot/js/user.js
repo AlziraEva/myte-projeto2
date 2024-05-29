08ed733b644e4497332b928896fe174e858ea0ec
Tarcila Cardoso Alves <t.cardoso.alves@avanade.com>
2024-05-29 20:54:37 -0300

    Ajustando user.js para a função copy

--- wwwroot/js/user.js
+++ wwwroot/js/user.js
@@ -1,25 +1,26 @@
-﻿
-function copyEmailAndPassword() {
-    /* Get the text fields */
+﻿function copyEmailAndPassword() {
+    /* Obtém os campos de texto */
     var email = document.getElementById("email");
     var password = document.getElementById("password");
 
-    /* Create a temporary textarea to hold the values */
+    /* Cria uma área de texto temporária para armazenar os valores */
     var tempInput = document.createElement("textarea");
     document.body.appendChild(tempInput);
 
-    /* Set the value of the textarea to the email and password */
+    /* Define o valor da textarea para o email e senha */
     tempInput.value = "Email: " + email.value + "\nPassword: " + password.value;
 
-    /* Select the text in the textarea */
-    tempInput.select();
+    /* Selecione o texto na área de texto */
+    tempInput.focus();
+    tempInput.setSelectionRange(0, tempInput.value.length);
 
-    /* Copy the text inside the textarea */
+    /* Copie o texto dentro da textarea */
     document.execCommand("copy");
 
-    /* Remove the temporary textarea */
-    document.body.removeChild(tempInput);
+    /* Remove a área de texto temporária */
+    document.body.removeChild(tempInput); 
 
-    /* Alert the copied text */
-    alert("Copied the text: " + tempInput.value);
 }
+
+
+
