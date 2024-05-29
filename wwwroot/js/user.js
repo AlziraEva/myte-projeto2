d1c033dd519619a3b05fe01c6b792497ff8ba568
Tarcila Cardoso Alves <t.cardoso.alves@avanade.com>
2024-05-29 17:00:02 -0300

    estilo da tela User

---
+++ wwwroot/js/user.js
@@ -0,0 +1,25 @@
+﻿
+function copyEmailAndPassword() {
+    /* Get the text fields */
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    /* Create a temporary textarea to hold the values */
+    var tempInput = document.createElement("textarea");
+    document.body.appendChild(tempInput);
+
+    /* Set the value of the textarea to the email and password */
+    tempInput.value = "Email: " + email.value + "\nPassword: " + password.value;
+
+    /* Select the text in the textarea */
+    tempInput.select();
+
+    /* Copy the text inside the textarea */
+    document.execCommand("copy");
+
+    /* Remove the temporary textarea */
+    document.body.removeChild(tempInput);
+
+    /* Alert the copied text */
+    alert("Copied the text: " + tempInput.value);
+}
