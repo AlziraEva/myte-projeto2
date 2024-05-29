dc8b13fed97dfaa7667defb5851ed5a4f2eab723
Tarcila Cardoso Alves <t.cardoso.alves@avanade.com>
2024-05-28 23:04:44 -0300

    View User, Create e Update

---
+++ Models/RegistroHoras.cs
@@ -0,0 +1,15 @@
+﻿namespace myte.Models
+{
+	public class RegistroHoras
+	{
+		public int FuncionarioId { get; set; }
+		public Funcionario Funcionario { get; set; } // Referência ao modelo Funcionario
+
+		public string CodigoWBS { get; set; }
+		public Wbs WBS { get; set; } // Referência ao modelo WBS
+
+		public DateTime Data { get; set; }
+		public int HorasTrabalhadas { get; set; }
+	}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
+
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using myte.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+//1° Adicionar o serviço de string de conexão com o servidor db
+builder.Services.AddDbContext<AppDbContext>((options) =>
+options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"])
+);
+//2° Contexto de autenticação
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+	.AddEntityFrameworkStores<AppDbContext>()
+	.AddDefaultTokenProviders();
+//Add services to the container.
 builder.Services.AddControllersWithViews();
+
+/***** Primeiro bloco unifica os serviços para a aplicação funcionar *****/
+
+
 
 var app = builder.Build();
 
@@ -13,6 +32,11 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+
+//3° Adiciona o método que auxilia na aplicação dos processos de autenticação de usuários para área restrita
+//UseAuthentication(); é um método
+app.UseAuthentication();
 
 app.UseAuthorization();
 
