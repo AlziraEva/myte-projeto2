ea1619a08ffb19de45caa3152e362dce788b0882
julia <juliadidra22@gmail.com>
2024-05-30 17:39:06 -0300

    services wbs e registroHoras, controller wbs

--- Controllers/WbsController.cs
+++ Controllers/WbsController.cs
@@ -1,51 +1,113 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using myte.Models;
+using myte.Services;
 
 namespace myte.Controllers
 {
     public class WbsController : Controller
     {
-        public IActionResult Index()
+        private WbsService _wbsService;
+
+        public WbsController(WbsService wbsService)
         {
-            return View(Repository.TodasAsWbs);
-            
+            _wbsService = wbsService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var wbs = await _wbsService.GetAllWbsAsync();
+                return View(wbs);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível listar os registros.");
+                return View(new List<Wbs>());
+            }
+
+            //return View(Repository.TodasAsWbs);
+
         }
 
-        public IActionResult CreateWbs()
+        //recupera uma unica wbs
+
+        /*public ViewResult GetWbsUnica() => View();
+
+        //sobrecarga
+
+        [HttpPost]
+        public async Task<ActionResult> GetWbsUnica(string codigo) //testar essa action sem o Iactionresult (para nao retornar uma view) na busca de wbs
         {
+            var wbs = await _wbsService.GetWbsByIdAsync(codigo);
+
+            if(wbs == null)
+            {
+                return NotFound();
+            }
+            return View(wbs);
+        }*/
+
+        public ViewResult CreateWbs() => View();
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateWbs(Wbs wbs)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _wbsService.AddWbsAsync(wbs);
+                    return RedirectToAction("Index");
+
+                    TempData["SuccessMessage"] = "Wbs cadastrada com sucesso!";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, "Erro ao criar registro");
+                }
+
+            }
+
+            return View(wbs);
+            TempData["ErrorMessage"] = "Não é possivel prosseguir com a ação";
+
+        }
+
+
+        public async Task<IActionResult> UpdateWbs(string codigo)
+        {
+            var wbs = await _wbsService.GetWbsByIdAsync(codigo);
+
+            if (wbs == null)
+            {
+                return NotFound();
+            }
+            return View(wbs);
+
+            /*Wbs consulta = Repository.TodasAsWbs.Where((r) => r.Codigo == id).First();
+            return View(consulta);*/
+        }
+
+        //sobrecarga
+        [HttpPost]
+        public async Task<IActionResult> UpdateWbs(string codigo, Wbs wbs) //alterei o nome de Create para CreateWbs
+        {
+
+            if (codigo != wbs.Codigo)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _wbsService.UpdateWbsAsync(codigo, wbs);
+                return Redirect("Index");
+            }
+
             return View();
-        }
 
-        [HttpPost]
-        public IActionResult CreateWbs(Wbs registroWbs)
-        {
-            if (ModelState.IsValid)
-            {
-                Repository.Inserir(registroWbs);
-                
-
-                TempData["SuccessMessage"] = "Wbs cadastrada com sucesso!";
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                TempData["ErrorMessage"] = "Não é possivel prosseguir com a ação";
-                return View();
-            }
-        }
-
-
-        public IActionResult UpdateWbs(string id)
-        {
-            Wbs consulta = Repository.TodasAsWbs.Where((r) => r.Codigo == id).First();
-            return View(consulta);
-        }
-
-        [HttpPost]
-        public IActionResult Update(string id, Wbs wbsAlterada)
-        {
-            
-            if (ModelState.IsValid)
+            /*if (ModelState.IsValid)
             {
                 var consulta = Repository.TodasAsWbs.Where((r) => r.Codigo == id).First();
 
@@ -54,28 +116,40 @@ namespace myte.Controllers
                 consulta.Tipo = wbsAlterada.Tipo;
                 consulta.Codigo = wbsAlterada.Codigo;
 
-                return Redirect("Index");
+                
             }
-            return View();
+           */
 
         }
 
         [HttpPost]
-        public IActionResult Delete(string id)
+        public async Task<IActionResult> DeleteWbs(string codigo)
         {
-            
-            try
+            var wbs = await _wbsService.GetWbsByIdAsync(codigo);
+
+            if (wbs == null)
+            {
+                return NotFound();
+            }
+
+            await _wbsService.DeleteWbsAsync(codigo);
+            return RedirectToAction(nameof(Index));
+
+            TempData["SuccessMessageDelete"] = "exclusao realizada com sucesso!";
+
+
+            /*try
             {
                 Wbs consulta = Repository.TodasAsWbs.Where((r) => r.Codigo == id).First();
                 Repository.Excluir(consulta);
-                TempData["SuccessMessageDelete"] = "exclusao realizada com sucesso!";
-                return RedirectToAction("Index");
+                
+                
             }
             catch
             {
                 TempData["ErrorMessageDelete"] = "Não é possivel prosseguir com a ação";
                 return Redirect("Index");
-            }
+            }*/
         }
     }
 }
