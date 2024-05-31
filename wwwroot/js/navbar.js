83961ae4d6a2d98a30a305377b321dba796591fe
Tarcila Cardoso Alves <t.cardoso.alves@avanade.com>
2024-05-30 22:32:17 -0300

    ajustei navbar e homer adm

---
+++ wwwroot/js/navbar.js
@@ -0,0 +1,11 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+    var controller = '@ViewContext.RouteData.Values["controller"].ToString()';
+    var navItems = document.querySelectorAll('.item');
+
+    navItems.forEach(function (navItem) {
+        if (navItem.getAttribute('data-controller') === controller) {
+            navItem.classList.add('active');
+        }
+    });
+});
